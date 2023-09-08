bd2203fa13d8b62e6f0dcb4bee804d049178125f
AriesJ12 <ajtagle12@gmail.com>
2023-09-08 21:31:18 +0800

    finished leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const DIVISIBLE = {four : 4, four_hundred: 400};
+    const NOT_DIVISIBLE = 100;
+    let result = false;
+    if (year%DIVISIBLE.four === 0)
+    {
+        result = true;
+    }
+    if (year%NOT_DIVISIBLE === 0)
+    {
+        result = false;
+    }
+    if (year%DIVISIBLE.four_hundred === 0)
+    {
+        result = true;
+    }
+    return result;
 };
 
 // Do not edit below this line
