f8474664120826ebeac147b6811e6e0846c829e0
AriesJ12 <ajtagle12@gmail.com>
2023-09-08 21:20:27 +0800

    finsiehd sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
 
+const sumAll = function(min, max) {
+    if(!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0)
+    {
+        return "ERROR";
+    }
+    if(min > max)
+    {
+        let reserve = min;
+        min = max;
+        max = reserve;
+    }
+    //uses iteration
+    let result = 0;
+    for(let i = min; i<= max; i++)
+    {
+        result += i;
+    }
+    return result;
+
+    //uses the formula for getting the sum all(algebra)
+    //return (max+min) * ((max - (min-1))/2)
 };
 
 // Do not edit below this line
