5170997a4ca9bc2596b875e24e516d2742740d4b
AriesJ12 <ajtagle12@gmail.com>
2023-09-20 10:13:28 +0800

    finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const arrWord = word.split("");
+    const reverse = arrWord.reverse().join("");
 
+    let result;
+    if(!word.localeCompare(reverse,'en', { sensitivity: 'base', ignorePunctuation: true }))
+    {
+        result = true;
+    }
+    else
+    {
+        result = false;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
