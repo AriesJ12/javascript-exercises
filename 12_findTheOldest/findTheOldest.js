380be60176ad9b7e930e3e4a62b9cc074028fa91
AriesJ12 <ajtagle12@gmail.com>
2023-09-20 11:06:15 +0800

    finished exercise 12(repo is done)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  return arr.reduce(function (result, person) {
+    if (Object.keys(result).length === 0) {
+      result = person;
+      return result;
+    }
+    const currentYear = new Date().getFullYear();
 
+    let currentAge = currentYear - result.yearOfBirth;
+    let nextAge = currentYear - person.yearOfBirth;
+    if (Object.hasOwn(result, "yearOfDeath")) {
+      currentAge = result.yearOfDeath - result.yearOfBirth;
+    }
+
+    if (Object.hasOwn(person, "yearOfDeath")) {
+      nextAge = person.yearOfDeath - person.yearOfBirth;
+    }
+
+    if (nextAge > currentAge) {
+      result = person;
+    }
+    return result;
+  }, {});
 };
 
 // Do not edit below this line
