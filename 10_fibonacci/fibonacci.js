c22d67176716dd30b5b9d0a69587df0cdfb43a09
AriesJ12 <ajtagle12@gmail.com>
2023-09-20 10:26:37 +0800

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(term) {
+    term = +term;
 
+    if (term <0 )
+    {
+        return "OOPS";
+    }
+    
+    if (term === 1)
+    {
+        return 1;
+    }
+    else if(term === 0)
+    {
+        return 0;
+    }
+    return fibonacci(term-1) + fibonacci(term-2) ;
 };
 
 // Do not edit below this line
